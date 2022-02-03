c537a6167c383f077d5ec0deed1b422a8748a769
major <majorofffpavel@gmail.com>
2022-02-03 18:07:31 +0300

    add directory with leetcode tasks

---
+++ Leetcode/needle in haystack.py
@@ -0,0 +1,10 @@
+#Дано. Найти вхождение подстроки в строку, вывести индекс первого вхождения
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+
+        if needle not in haystack:
+            return -1
+        if len(needle) == 0:
+            return 0
+        return haystack.index(needle)
